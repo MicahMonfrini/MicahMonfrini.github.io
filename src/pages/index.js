b69ab5edc0064f25f94f6f5f482448d14efe274d
MicahMonfrini <ffrock905@gmail.com>
2017-08-16 22:26:16 -0500

    create landing page stylesheet and reset default styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ function IndexPage() {
         <meta charSet="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
+        <link rel="stylesheet" href="/static/landing.css"/>
         <title>Micah Monfrini</title>
       </Head>
       <h1 className="header">
