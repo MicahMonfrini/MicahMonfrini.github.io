c762035edb26bcac5bbf5c500827a90e8360f350
MicahMonfrini <ffrock905@gmail.com>
2017-08-16 22:24:14 -0500

    add landing page content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-// import Link from 'next/link';
+import Link from 'next/link';
 
 function IndexPage() {
   return (
@@ -11,6 +11,24 @@ function IndexPage() {
         <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
         <title>Micah Monfrini</title>
       </Head>
+      <h1 className="header">
+        Micah Monfrini
+      </h1>
+      <p className="headerText">
+        UI/UX Developer --- Front-End Developer
+      </p>
+      <Link href="/about" className="navButton">
+        About
+      </Link>
+      <Link href="/projects" className="navButton">
+        Projects
+      </Link>
+      <Link href="/contact" className="navButton">
+        Contact
+      </Link>
+      <div className="socialNav">
+        icons go here
+      </div>
     </div>
   );
 }
