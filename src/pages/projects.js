bb8746c1e0449e5b9bc87c1037ff0c32af2d72c8
MicahMonfrini <ffrock905@gmail.com>
2017-08-21 19:28:53 -0500

    import new stylesheets

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -9,7 +9,7 @@ function ProjectsPage() {
         <meta charSet="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
-        <link rel="stylesheet" href=""/>
+        <link rel="stylesheet" href="../static/css/projects.css"/>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100"/>
         <link rel="stylesheet" href="../static/css/font-awesome/css/font-awesome.min.css"/>
         <title>Projects</title>
