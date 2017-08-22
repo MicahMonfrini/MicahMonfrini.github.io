020dfea9717ebf8f3f84f594a46d2ffae4100626
MicahMonfrini <ffrock905@gmail.com>
2017-08-22 18:50:08 -0500

    add test text

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,6 +15,13 @@ function AboutPage() {
         <title>About</title>
       </Head>
       <h1>About</h1>
+      <p className="testText">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Aliquid reiciendis magni ipsa, est vitae iure laboriosam commodi eius
+        suscipit sequi quod maiores dicta, ex, in quia nesciunt animi
+        dolores voluptas.
+      </p>
+      <p className="testText" style={{marginTop: 100}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit tempore unde voluptas suscipit dicta deserunt temporibus culpa molestias nesciunt nemo veritatis iure dolores labore assumenda quas reiciendis, sit consectetur porro tenetur commodi iusto perferendis quaerat libero. Quis veritatis ex aspernatur aperiam reprehenderit vero cum, neque distinctio beatae, ipsum tempore quibusdam?</p>
     </div>
   );
 }
