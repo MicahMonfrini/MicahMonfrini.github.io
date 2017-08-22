fb33a3eef3db81329bfa17c0db2f99e589796d79
MicahMonfrini <ffrock905@gmail.com>
2017-08-21 19:06:32 -0500

    add head

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,3 +1,21 @@
 import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
+
+function ContactPage() {
+  return (
+    <div>
+      <Head>
+        <meta charSet="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
+        <link rel="stylesheet" href=""/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100"/>
+        <link rel="stylesheet" href="../static/css/font-awesome/css/font-awesome.min.css"/>
+        <title>Contact</title>
+      </Head>
+    </div>
+  );
+}
+
+export default ContactPage;
