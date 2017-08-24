8509ebb7672139ba7d1686e9269cd1a50cef21f6
MicahMonfrini <ffrock905@gmail.com>
2017-08-24 17:23:19 -0500

    add placeholders for main bio content

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,20 +15,27 @@ function AboutPage() {
         <title>About</title>
       </Head>
       <Header />
-      <h1>About</h1>
-      <p className="testText">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Aliquid reiciendis magni ipsa, est vitae iure laboriosam commodi eius
-        suscipit sequi quod maiores dicta, ex, in quia nesciunt animi
-        dolores voluptas.
-      </p>
-      <p className="testText" style={{marginTop: 50}}>Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Fugit tempore unde voluptas suscipit dicta
-        deserunt temporibus culpa molestias nesciunt nemo veritatis iure dolores
-        labore assumenda quas reiciendis, sit consectetur porro tenetur commodi
-        iusto perferendis quaerat libero. Quis veritatis ex aspernatur aperiam
-        reprehenderit vero cum, neque distinctio beatae, ipsum tempore quibusdam?
-      </p>
+      <div className="bio">
+        <div className="bioImg">
+          <img src="http://lorempixel.com/output/cats-q-c-507-480-5.jpg"/>
+        </div>
+        <div className="bioText">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Aliquid reiciendis magni ipsa, est vitae iure laboriosam commodi eius
+            suscipit sequi quod maiores dicta, ex, in quia nesciunt animi
+            dolores voluptas.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Fugit tempore unde voluptas suscipit dicta
+            deserunt temporibus culpa molestias nesciunt nemo veritatis iure dolores
+            labore assumenda quas reiciendis, sit consectetur porro tenetur commodi
+            iusto perferendis quaerat libero. Quis veritatis ex aspernatur aperiam
+            reprehenderit vero cum, neque distinctio beatae, ipsum tempore quibusdam?
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
