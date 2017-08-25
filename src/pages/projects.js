03b8abad597a180de03d7bbb8854b4756056c369
MicahMonfrini <ffrock905@gmail.com>
2017-08-25 13:23:03 -0500

    add Header component to projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import Link from 'next/link';
+import Header from '../components/Header/Header.js';
 
 function ProjectsPage() {
   return (
@@ -14,7 +14,7 @@ function ProjectsPage() {
         <link rel="stylesheet" href="../static/css/font-awesome/css/font-awesome.min.css"/>
         <title>Projects</title>
       </Head>
-      <h1>Projects</h1>
+      <Header />
     </div>
   );
 }
