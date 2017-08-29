3426c4edb74fd59687e2f0b52c1ca547ea5aff7c
MicahMonfrini <ffrock905@gmail.com>
2017-08-28 20:13:03 -0500

    add Head to Footer component/create Footer.css file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,13 @@ import Head from 'next/head';
 
 function Footer() {
   return (
-    <Head></Head>
+    <Head>
+      <meta charSet="UTF-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
+      <link rel="stylesheet" href="../static/css/Footer.css"/>
+      <link rel="stylesheet" href="../static/css/font-awesome/css/font-awesome.min.css"/>
+    </Head>
   );
 }
 
