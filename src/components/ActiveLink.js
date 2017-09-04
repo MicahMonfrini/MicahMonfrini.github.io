ec5814d4eea48512ba4bc41bb776a12b230ee636
MicahMonfrini <ffrock905@gmail.com>
2017-09-04 13:05:27 -0500

    added style to ActiveLink component

--- src/components/ActiveLink.js
+++ src/components/ActiveLink.js
@@ -5,8 +5,14 @@ import { withRouter } from 'next/router';
 const ActiveLink = ({ children, router, href }) => {
   const style = {
     textDecoration: 'none',
+    textTransform: 'uppercase',
+    fontSize: 20,
+    fontWeight: 100,
+    margin: 15,
+    paddingBottom: 5,
+    position: 'relative',
     marginRight: 10,
-    color: router.pathname === href ? 'red' : 'black'
+    color: router.pathname === href ? 'red' : 'white'
   }
 
   const handleClick = (e) => {
