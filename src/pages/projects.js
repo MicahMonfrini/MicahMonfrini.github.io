1a28e2c7152d84fc719361264570a8fa5f083b26
MicahMonfrini <ffrock905@gmail.com>
2017-09-09 18:51:33 -0500

    add Footer to projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function ProjectsPage() {
   return (
@@ -15,6 +16,7 @@ function ProjectsPage() {
         <title>Projects</title>
       </Head>
       <Header />
+      <Footer />
     </div>
   );
 }
